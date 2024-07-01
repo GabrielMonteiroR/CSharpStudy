e82baa842e6c4cf77a4314040fefdf601d8bb67d
Gabriel <gmonteiro810@gmail.com>
2024-07-01 14:16:55 -0300

    main

---
+++ thirty_eight/Models/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Triangle
+{
+    public double a;
+    public double b;
+    public double c;
+
+    public Triangle(double a, double b, double c)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double Area()
+    {
+        double p = (a + b + c) / 2;
+        double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
+}
---
+++ thirty_eight/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Triangle triangleOne = new Triangle(3.0, 4.0, 5.0);
+        double area = triangleOne.Area();
+        Console.WriteLine("Área do triângulo: " + area);
+    }
+}
